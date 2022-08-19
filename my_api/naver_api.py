c8b529d54da30813bd9ce44132c774affc4d199a
HEEpage <page.0895059@gmail.com>
2022-08-19 14:41:44 +0900

    modify: naver_api

--- my_api/naver_api.py
+++ my_api/naver_api.py
@@ -5,11 +5,14 @@ def search_api(url, client_id, client_secret, params):
         "X-Naver-Client-Id" : client_id,
         "X-Naver-Client-Secret" : client_secret
     }
+
     res = requests.get(url,params=params,headers=headers)
     result = res.json()
+
     if res.status_code != 200:
         print(result)
         result = None
+
     return result
 
 def translate_api(text, url, client_id, client_secret, source="ko", target="en"):
@@ -22,8 +25,11 @@ def translate_api(text, url, client_id, client_secret, source="ko", target="en")
         "target": target, # 목적언어 코드
         "text" : text # 번역할 텍스트
     }
+
     res = requests.post(url,headers=headers,data=data)
     result = res.json()
+
     if res.status_code == 200:
         result = result["message"]["result"]["translatedText"]
+        
     return result
